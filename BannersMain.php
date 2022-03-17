36657d786ab5ca41b20ec3001abf3aca88db0ed9
andergmartins <anderson@publishpress.com>
2022-03-16 21:54:41 -0300

    Avoid redefining the class if it is already defined

--- BannersMain.php
+++ BannersMain.php
@@ -24,98 +24,100 @@ namespace PublishPress\WordPressBanners;
 
 use Exception;
 
-define('PP_WP_BANNERS_VERSION', '1.2.6');
+define('PP_WP_BANNERS_VERSION', '1.2.7');
 
-class BannersMain
-{
+if (! class_exists('\\PublishPress\\WordPressBanners\\BannersMain')) {
+    class BannersMain
+    {
 
-    /**
-     * HTML output banner inviting to install another PublishPress plugin or advertise a feature
-     *
-     * @param string    $heading        Custom heading; disabled if blank
-     * @param string    $title          Custom title; disabled if blank
-     * @param array     $content        Content to display. e.g. Feature list or a single paragraph
-     * @param string    $link           Link to apply to button and image
-     * @param string    $link_title     Link title
-     * @param string    $image          A filename from assets/images/ folder; disabled if blank
-     *
-     * @return void
-     */
-    public function pp_display_banner(
-        $heading = '',
-        $title = '',
-        $contents = array(),
-        $link = '',
-        $link_title = '',
-        $image = ''
-    ) {
-        if( !empty($heading) ) {
-            ?>
-			<p class="nav-tab-wrapper pp-recommendations-heading">
-				<?php echo $heading ?>
-			</p>
-            <?php
-        }
-        ?>
-
-		<div class="pp-sidebar-box">
-
-            <?php
-            if( !empty($title) ) {
+        /**
+         * HTML output banner inviting to install another PublishPress plugin or advertise a feature
+         *
+         * @param string    $heading        Custom heading; disabled if blank
+         * @param string    $title          Custom title; disabled if blank
+         * @param array     $content        Content to display. e.g. Feature list or a single paragraph
+         * @param string    $link           Link to apply to button and image
+         * @param string    $link_title     Link title
+         * @param string    $image          A filename from assets/images/ folder; disabled if blank
+         *
+         * @return void
+         */
+        public function pp_display_banner(
+            $heading = '',
+            $title = '',
+            $contents = array(),
+            $link = '',
+            $link_title = '',
+            $image = ''
+        ) {
+            if( !empty($heading) ) {
                 ?>
-				<h3>
-					<?php echo $title ?>
-				</h3>
+                <p class="nav-tab-wrapper pp-recommendations-heading">
+                    <?php echo $heading ?>
+                </p>
                 <?php
             }
+            ?>
 
-            if( count($contents) > 1 ) {
-                ?>
-				<ul>
-                    <?php
-                    foreach($contents as $content) {
-                        ?>
-    					<li>
-    						<?php echo $content; ?>
-    					</li>
-                        <?php
-                    }
+            <div class="pp-sidebar-box">
+
+                <?php
+                if( !empty($title) ) {
                     ?>
-				</ul>
-                <?php
-            } else {
-                ?>
-                <p><?php echo $contents[0] ?></p>
-                <?php
-            }
-            ?>
+                    <h3>
+                        <?php echo $title ?>
+                    </h3>
+                    <?php
+                }
 
-			<p>
-				<a class="button button-primary" href="<?php echo $link ?>">
-					<?php echo $link_title ?>
-				</a>
-			</p>
+                if( count($contents) > 1 ) {
+                    ?>
+                    <ul>
+                        <?php
+                        foreach($contents as $content) {
+                            ?>
+                            <li>
+                                <?php echo $content; ?>
+                            </li>
+                            <?php
+                        }
+                        ?>
+                    </ul>
+                    <?php
+                } else {
+                    ?>
+                    <p><?php echo $contents[0] ?></p>
+                    <?php
+                }
+                ?>
+
+                <p>
+                    <a class="button button-primary" href="<?php echo $link ?>">
+                        <?php echo $link_title ?>
+                    </a>
+                </p>
+
+                <?php
+                if(!empty($image) && file_exists(__DIR__  . '/assets/images/' . $image)) {
+                    ?>
+                    <div class="pp-box-banner-image">
+                        <a href="<?php echo $link ?>">
+                            <img src="<?php echo plugin_dir_url(__FILE__) . 'assets/images/' . $image ?>" title="<?php echo $title ?>" />
+                        </a>
+                    </div>
+                    <?php
+                }
+                ?>
+
+            </div>
 
             <?php
-            if(!empty($image) && file_exists(__DIR__  . '/assets/images/' . $image)) {
-                ?>
-				<div class="pp-box-banner-image">
-					<a href="<?php echo $link ?>">
-						<img src="<?php echo plugin_dir_url(__FILE__) . 'assets/images/' . $image ?>" title="<?php echo $title ?>" />
-					</a>
-				</div>
-                <?php
-            }
-            ?>
-
-		</div>
-
-		<?php
-        wp_enqueue_style(
-            'pp-wordpress-banners-style',
-            plugin_dir_url( __FILE__ ) . 'assets/css/style.css',
-            false,
-            PP_WP_BANNERS_VERSION
-        );
-	}
+            wp_enqueue_style(
+                'pp-wordpress-banners-style',
+                plugin_dir_url( __FILE__ ) . 'assets/css/style.css',
+                false,
+                PP_WP_BANNERS_VERSION
+            );
+        }
+    }
 }
