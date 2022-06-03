f14280ef1a8eb1de57fd1165eba777f5c01f3849
Valentín García <htmgarcia@gmail.com>
2022-06-03 10:41:39 -0500

    Increase version

--- BannersMain.php
+++ BannersMain.php
@@ -25,7 +25,7 @@ namespace PublishPress\WordPressBanners;
 use Exception;
 
 if (! defined('PP_WP_BANNERS_VERSION')) {
-    define('PP_WP_BANNERS_VERSION', '1.2.9');
+    define('PP_WP_BANNERS_VERSION', '1.2.10');
 }
 
 if (! class_exists('\\PublishPress\\WordPressBanners\\BannersMain')) {
